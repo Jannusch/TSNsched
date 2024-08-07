42df1ab77a2e832208d2d9989185a23ede68fbcd
Jannusch Bigge <jannusch.bigge@tu-dresden.de>
2024-08-07 14:27:26 +0200

    gRPC Server for TSNsched

--- src/main/java/com/tsnsched/grpc/TSNschedServer.java
+++ src/main/java/com/tsnsched/grpc/TSNschedServer.java
@@ -9,14 +9,26 @@ import io.grpc.InsecureServerCredentials;
 import io.grpc.Server;
 import io.grpc.stub.StreamObserver;
 
+import com.tsnsched.core.interface_manager.JSONParser;
+import com.tsnsched.core.network.*;
+import com.tsnsched.core.schedule_generator.*;
+import com.tsnsched.core.interface_manager.*;
+
 public class TSNschedServer {
 
     private static final Logger logger = Logger.getLogger(TSNschedServer.class.getName());
     private Server server;
 
-    public static void main(String[] args)  throws IOException{
+    public static void main(String[] args)  throws IOException, InterruptedException {
         final TSNschedServer server = new TSNschedServer();
         server.start();
+        server.blockUntilShutdown();
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
     }
 
 
@@ -27,16 +39,20 @@ public class TSNschedServer {
                 .build()
                 .start();
         logger.info("Server started, listenig on " + port);
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.err.println("*** shutting down gRPC server since JVM is shutting down");
-            try {
-                TSNschedServer.this.stop();
-            } catch (InterruptedException e) {
-                e.printStackTrace(System.err);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                try {
+                    TSNschedServer.this.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                }
+                System.err.println("*** server shut down");
             }
-            System.err.println("*** server shut down");
-}));
+        });
     }
+
 
     private void stop() throws InterruptedException {
         if (server != null) {
@@ -47,7 +63,14 @@ public class TSNschedServer {
     static class SchedulerImpl extends SchedulerGrpc.SchedulerImplBase {
         @Override
         public void createSchedule(ScheduleInput req, StreamObserver<ScheduleOutput> responseObserver) {
-            ScheduleOutput rply = ScheduleOutput.newBuilder().setOutput("Return from java").build();
+            // create new schedule
+            JSONParser parser = new JSONParser("NO FILE");
+            Network net = parser.parseInputContent(req.getInput());
+            ScheduleGenerator gen = new ScheduleGenerator();
+            gen.generateSchedule(net);
+            String result = parser.generateOutputToString(net);
+
+            ScheduleOutput rply = ScheduleOutput.newBuilder().setOutput(result).build();
             responseObserver.onNext(rply);
             responseObserver.onCompleted();
         }
